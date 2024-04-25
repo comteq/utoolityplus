7af6b64c615b2554bd84fff174cb398a7520a804
Robert Pogi G Bisnar Jr <127765784+robertpogi@users.noreply.github.com>
2024-04-26 00:58:24 +0800

    flatpicker and datepicker fix offline

--- resources/views/admin/schedule.blade.php
+++ resources/views/admin/schedule.blade.php
@@ -1,7 +1,8 @@
 @include('nav')
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="stylesheet" href="{{ asset('css/external-styles.css') }}">
-
+<link rel="stylesheet" href="{{ asset('js/btdatepicker.min.css') }}">
+<link rel="stylesheet" href="{{ asset('js/flatpickr.min.css') }}">
 <style>
     .card-container {
         display: flex;
@@ -437,8 +438,6 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/js/select2.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<script src="{{ asset('js/sweetalert2@10.js') }}"></script>
-
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
@@ -447,20 +446,15 @@
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="{{ asset('js/dynamicUpdate.js') }}"></script>
-
-<script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
-
-{{-- <script src="{{ asset('js/flatpickr.min.js') }}"></script> --}}
-
-<script src="{{ asset('css/datepicker.min.css') }}"></script> 
-
-<script src="{{ asset('css/flatpickr.min.css') }}"></script> 
-
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="https://netdna.bootstrapcdn.com/bootstrap/2.3.2/js/bootstrap.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/js/bootstrap-datepicker.min.js"></script>
 
+<!-- <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script> -->
+
+<script src="{{ asset('js/btdatepicker.min.js') }}"></script> 
+<script src="{{ asset('js/flatpickr.min.js') }}"></script> 
 <!-- __________________________________________________pickers__________________________________________________________________-->
 
 <script>
@@ -469,7 +463,7 @@
             format: "mm-yyyy",
             startView: "months", 
             minViewMode: "months",
-            autoclose:true,
+          
             // onSelect: calculateDays
         });   
     })
