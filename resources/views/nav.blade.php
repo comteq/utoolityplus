561ac1d486c21b539afe17f2d12663b1ae321879
Robert Pogi G Bisnar Jr <127765784+robertpogi@users.noreply.github.com>
2024-05-17 22:51:00 +0800

    remove notif

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -150,18 +150,19 @@
   <img src="{{ asset('images/logo-inline.png') }}" height="60px" class="navbar-brand">
 
   <div class="navbar-header">
+
   @if(auth()->user() && auth()->user()->role !== 'user')
     <!-- Notification bell -->
     <div class="customcontainer notification-container2">
     <!-- Notification bell -->
  
-    <div id="openNotificationBtn">
+    <!-- <div id="openNotificationBtn">
         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-bell" viewBox="0 0 16 16">
             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
         </svg>
         <span id="notification-count">0</span>
     </div>
-    <div class="notification-window" id="notificationWindow"></div>
+    <div class="notification-window" id="notificationWindow"></div> -->
   </div>
   @endif
 
@@ -201,7 +202,7 @@
 
     @if(auth()->user() && auth()->user()->role !== 'user')
     <!-- Notification bell -->
-    <div class="customcontainer notification-container">
+    <!-- <div class="customcontainer notification-container">
     <div id="openNotificationBtns">
         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-bell" viewBox="0 0 16 16">
             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
@@ -209,7 +210,7 @@
         <span id="notification-counts">0</span>
     </div>
     <div class="notification-window" id="notificationWindows"></div>
-</div>
+</div> -->
 <!-- End notification bell -->
     @endif
 
